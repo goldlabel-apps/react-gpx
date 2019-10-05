3683968af07ece37c648f35840d21926383a36a6
listingslab <listingslab@gmail.com>
2019-10-05 19:57:18 +1000

    Clean up create react app

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <h1>GPX Replay</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/listingslab/gpx-replay"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub
         </a>
       </header>
     </div>
